a25aaf4ba0bba30930705678416f1f4457874da8
DanielG2027 <d.garcia8021@gmail.com>
2026-01-12 18:34:27 -0800

    Changed main, added collision detection

--- main.py
+++ main.py
@@ -8,10 +8,17 @@ def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
     print(f"Screen width: {SCREEN_WIDTH} \nScreen height: {SCREEN_HEIGHT}")
     
+    game_clock = pygame.time.Clock()
+    dt = 0
+    
     while True:
         log_state()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
         screen.fill("black")
         pygame.display.flip()
-        
+        game_clock.tick(60)
+        dt = game_clock.tick(60)/1000
 if __name__ == "__main__":
     main()
